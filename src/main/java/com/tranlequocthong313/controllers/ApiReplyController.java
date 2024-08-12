df26ce26a77b2e8cf26fb0865b1b6f23fcbfcdb6
tranlequocthong313 <tranlequocthong313@gmail.com>
2024-08-12 20:58:55 +0700

    fix: add cross origin

--- src/main/java/com/tranlequocthong313/controllers/ApiReplyController.java
+++ src/main/java/com/tranlequocthong313/controllers/ApiReplyController.java
@@ -16,41 +16,33 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author tranlequocthong313
  */
 @RestController
 @RequestMapping("/api/replies")
+@CrossOrigin
 public class ApiReplyController {
-	
+
 	@Autowired
 	private ReplyService replyService;
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping
 	public List<ReplyDto> getReplies(@RequestParam Map<String, String> queryParams) {
 		return replyService.findAll(queryParams);
 	}
-	
+
 	@GetMapping("/{id}")
 	public ReplyDto getReply(@PathVariable(value = "id") Integer id) {
 		ReplyDto reply = replyService.findById(id);
 		return reply;
 	}
-	
+
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.CREATED)
 	public Reply createReply(@Valid @RequestBody Reply reply) {
@@ -58,7 +50,7 @@ public class ApiReplyController {
 		replyService.save(reply);
 		return reply;
 	}
-	
+
 	@PatchMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.OK)
 	public Reply updateReply(@Valid @RequestBody Reply reply, @PathVariable(value = "id") int id) {
@@ -66,7 +58,7 @@ public class ApiReplyController {
 		replyDto.setContent(reply.getContent());
 		return replyService.update(replyDto);
 	}
-	
+
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteReply(@PathVariable(value = "id") int id) {
